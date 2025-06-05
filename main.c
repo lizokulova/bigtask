6cd6b9c312f7c99fbbc2d0310257e1db007c7c3d
lizokulova <163635310+lizokulova@users.noreply.github.com>
2025-06-05 13:34:02 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,91 @@
 #include <math.h> 
 #include "lodepng.h" 
 
+typedef struct DSU{
+    int *parent;
+    int *rank;
+    int size;
+}DSU;
+
+// Инициализация DSU
+void initDSU(DSU *dsu, int size){
+    dsu->parent = (int *)malloc(size*sizeof(int));
+    dsu->rank = (int *)malloc(size*sizeof(int));
+    dsu->size = size;
+    for (int i = 0; i < size; i++) {
+        dsu->parent[i] = i;
+        dsu->rank[i] = 0;
+    }
+}
+// Найти корень элемента с path compression
+int findRoot(DSU *dsu, int x) {
+    if (dsu->parent[x] != x) {
+        dsu->parent[x] = findRoot(dsu, dsu->parent[x]);
+    }
+    return dsu->parent[x];
+}
+// Объединить два множества
+void unionSets(DSU *dsu, int x, int y) {
+    int rootX = findRoot(dsu, x);
+    int rootY = findRoot(dsu, y);
+    if (rootX == rootY) return;
+    if (dsu->rank[rootX] > dsu->rank[rootY]){
+        dsu->parent[rootY] = rootX;
+    } else {
+        dsu->parent[rootX] = rootY;
+        if (dsu->rank[rootX] == dsu->rank[rootY]) {
+            dsu->rank[rootY]++;
+        }
+    }
+}
+
+// Проверка, являются ли два цвета "примерно одинаковыми"
+int isSimilar(unsigned char a, unsigned char b, int threshold) {
+    return abs(a - b) <= threshold;
+}
+
+// Найти компоненты связности в изображении
+void findConnectedComponents(unsigned char *bw_pic, int width, int height, int threshold) {
+    int size = width * height;
+    DSU dsu;
+    initDSU(&dsu, size);
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int current_idx = y * width + x;
+            if (x + 1 < width) {
+                int right_idx = y * width + (x + 1);
+                if (isSimilar(bw_pic[current_idx], bw_pic[right_idx], threshold)) {
+                    unionSets(&dsu, current_idx, right_idx);
+                }
+            }
+            if (y + 1 < height) {
+                int bottom_idx = (y + 1) * width + x;
+                if (isSimilar(bw_pic[current_idx], bw_pic[bottom_idx], threshold)) {
+                    unionSets(&dsu, current_idx, bottom_idx);
+                }
+            }
+        }
+    }
+
+    int *component_sizes = (int *)calloc(size, sizeof(int));
+    for (int i = 0; i < size; i++) {
+        int root = findRoot(&dsu, i);
+        component_sizes[root]++;
+    }
+    
+    // Вывести информацию о компонентах (для примера)
+    int num_components = 0;
+    for (int i = 0; i < size; i++) {
+        if (component_sizes[i] > 0) {
+            num_components++;
+        }
+    }
+    printf("Total connected components: %d\n", num_components);
+    
+    free(component_sizes);
+    free(dsu.parent);
+    free(dsu.rank);
+}
 
 unsigned char* load_png(const char* filename, unsigned int* width, unsigned int* height) 
 {
@@ -35,7 +120,14 @@ void rgba_to_bw(unsigned char *bw_pic, insigned char *rgb_pic, int width, int he
   }
   return ;
 }
-
+//перевод картинки из черно-белой в RGB
+void bw_to_rgba(unsigned char *bw_pic, insigned char *rgb_pic, int width, int height){
+  int i;
+  for(i=0; i<width*height; i++){
+    bw_pic[i] = 0.3*rgb_pic[4*i] + 0.59*rgb_pic[4*i+1] + 0.11*rgb_pic[4*i+2];
+  }
+  return ;
+}
 // вариант огрубления серого цвета в ЧБ с заданными верхним и нижним диапазоном огрубления в main
 void contrast(unsigned char *bw_pic, int width, int height, int bottom, int top){ 
     int i; 
@@ -79,8 +171,8 @@ void Gauss_blur(unsigned char *col, unsigned char *blr_pic, int width, int heigh
 //оператор Робертса
 void roberts_alg(unsigned char *bw_pic, int width, int height) {
     unsigned char *edges = (unsigned char *)malloc(width * height);
-    const int Gx_kernel[2][2] = {{1, 0}, {0, -1}};  // Диагональ \
-    const int Gy_kernel[2][2] = {{0, 1}, {-1, 0}};  // Диагональ /
+    int Gx_kernel[2][2] = {{1, 0}, {0, -1}};  // Диагональ \
+    int Gy_kernel[2][2] = {{0, 1}, {-1, 0}};  // Диагональ /
     int x, y;
     for (y = 0; y < height-1; y++) {
         for (x = 0; x < width-1; x++) {
@@ -138,21 +230,38 @@ int main()
     size = width * height * 4;
     bw_size = width * height;
     
-    
     unsigned char* bw_pic = (unsigned char*)malloc(bw_size*sizeof(unsigned char)); 
     unsigned char* blr_pic = (unsigned char*)malloc(bw_size*sizeof(unsigned char)); 
     unsigned char* finish = (unsigned char*)malloc(size*sizeof(unsigned char)); 
- 
+
+    rgba_to_bw(bw_pic, picture, width, height);
+
     // Например, поиграли с  контрастом
-    contrast(bw_pic, bw_size); 
+    contrast(bw_pic, width, height, 30, 200); 
+    
         // посмотрим на промежуточные картинки
-    write_png("contrast.png", finish, width, height);
+    //write_png("contrast.png", finish, width, height);
     
     // поиграли с Гауссом
     Gauss_blur(bw_pic, blr_pic, width, height); 
+    color(blr_pic, finish, size);
     // посмотрим на промежуточные картинки
     write_png("gauss.png", finish, width, height);
-    
+/*
+    // Пример использования
+      int width = 5;
+      int height = 5;
+      unsigned char bw_pic[] = {
+          100, 100, 105, 200, 200,
+          100, 102, 110, 200, 200,
+          100, 100, 105, 200, 200,
+          50,  50,  55, 210, 210,
+          50,  50,  50, 210, 210
+      };
+      
+      int threshold = 10; // Максимальная разница в цвете
+      
+      findConnectedComponents(bw_pic, width, height, threshold);
     // сделали еще что-нибудь
     // .....
     // ....
@@ -168,7 +277,7 @@ int main()
     
     // выписали результат
     write_png("picture_out.png", finish, width, height); 
-    
+    */
     // не забыли почистить память!
     free(bw_pic); 
     free(blr_pic); 
